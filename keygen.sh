6b90478a6ee722c81ffff70a738473425b63d7ff
Adam Russo <adam.jordan.russo@gmail.com>
2026-06-10 08:43:43 -0400

    tweaked keygen script

--- keygen.sh
+++ keygen.sh
@@ -1,4 +1,12 @@
-# One-time manual step
 ssh-keygen -t rsa -b 4096 -f logger-key.pem
-aws ssm put-parameter --name "/dev/logger/private-key" --type SecureString --value file://logger-key.pem
-aws ec2 import-key-pair --key-name "logger-key" --public-key-material fileb://logger-key.pem.pub
+
+aws ssm put-parameter \
+  --name "/dev/logger/private-key" \
+  --type SecureString \
+  --value file://logger-key.pem \
+  --region us-east-1
+
+aws ec2 import-key-pair \
+  --key-name "logger-key" \
+  --public-key-material fileb://logger-key.pem.pub \
+  --region us-east-1
